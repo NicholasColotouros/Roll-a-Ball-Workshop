079af3bb854a966d239a9ca8539ed242ad8d3021
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2016-11-21 10:03:54 -0500

    Parts 1-4 of the tutorial

---
+++ Roll A workshop/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+	// This is called at regular intervals
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+}
